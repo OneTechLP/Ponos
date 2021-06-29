49baeac182bfc0a78cf817dced637384c51d0c4a
Clayton Reeves <Clayton.reeves@utahtechsolutions.com>
2021-06-29 09:21:09 -0500

    Failing Test

--- Sln/Tests/ProductReceiptUnitTest.cs
+++ Sln/Tests/ProductReceiptUnitTest.cs
@@ -1,9 +1,22 @@
 ﻿
 
+using System.Collections.Generic;
+using Prog;
+using Xunit;
+
 namespace Tests
 {
     public class ProductReceiptUnitTest
     {
-        
+        [Fact]
+        public void EmptyReceipt()
+        {
+            string[] values = { };
+            var expectedValue = new List<Product>();
+
+            var actualValue = ConvertReceiptToEnumerable(values);
+
+            actualValue.Should().BeEquivalentTo(expectedValue);
+        }
     }
 }
