c2e0486ab1c7f93c0c13c1b688550f5dba87fc13
Clayton Reeves <Clayton.reeves@utahtechsolutions.com>
2021-06-29 09:29:52 -0500

    Failing Test

--- Sln/Tests/ProductReceiptUnitTest.cs
+++ Sln/Tests/ProductReceiptUnitTest.cs
@@ -12,12 +12,32 @@ namespace Tests
         [Fact]
         public void EmptyReceipt()
         {
-            string[] values = { };
-            var expectedValue = new List<Product>();
+            string[] values =
+            {
+                "Some Store",
+                "Some Product    1520256466              25.52 D",
+                "                                    TOTAL 25.52"
+            };
+            var expectedValue = new List<Product>
+            {
+                new ()
+                {
+                    Price = 25.52m,
+                    Code = "1520256466"
+                }
+            };
 
             var actualValue = Product.ConvertReceiptToEnumerable(values);
 
             actualValue.Should().BeEquivalentTo(expectedValue);
+        }
+
+        [Fact]
+        public void SimpleReceiptWith1Item()
+        {
+            string[] values = { };
+            var expectedValue = new List<Product>();
+
         }
     }
 }
