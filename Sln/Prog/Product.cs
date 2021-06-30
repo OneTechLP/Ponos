689438db2e2a463c52d02d704ed747a9e52ce949
Clayton Reeves <Clayton.reeves@utahtechsolutions.com>
2021-06-29 22:07:57 -0500

    Setting up Reading from a file

--- Sln/Prog/Product.cs
+++ Sln/Prog/Product.cs
@@ -2,15 +2,18 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using 
+
 
 namespace Prog
 {
     public class Product
     {
-        public string Code {get;set;}
-        public decimal Price {get;set;}
+        public string Code { get; set; }
+        public decimal Price { get; set; }
 
         public static async Task<IEnumerable<Product>> ReadPricesFromFile(string path)
         {
@@ -22,26 +25,36 @@ namespace Prog
         public static IEnumerable<Product> ConvertPricesToEnumberable(string[] values)
         {
             return values.Select(product => product.Split(','))
-                .Select(product => new Product 
+                .Select(product => new Product
                 {
                     Code = product[0],
                     Price = Convert.ToDecimal(product[1])
                 });
         }
 
-        public static async Task<IEnumerable<Product>> ReadReceiptFromFile(string path)
+        public static async Task<IEnumerable<Product>> ReadReceiptFromFile(string directoryPath)
         {
-            var fileContents = await ReadFromFile(path);
+            var files = Directory.GetFiles(directoryPath, "*", SearchOption.AllDirectories);
 
-            return ConvertReceiptToEnumerable(fileContents);
+            var receipts = new List<Product>();
+
+            foreach (var file in files)
+            {
+                if (file.Contains("PaxHeader")) continue;
+                var fileContents = await ReadFromFile(file);
+
+                receipts.AddRange(ConvertReceiptToEnumerable(fileContents));
+            }
+
+            return receipts;
         }
 
         public static IEnumerable<Product> ConvertReceiptToEnumerable(string[] values)
         {
             var usefulLines = values.Skip(1).SkipLast(1);
-            
+
             var regex = new Regex(@"\d+(\.\d+)?");
-            
+
             var receiptProductList = new List<Product>();
 
             foreach (var line in usefulLines)
@@ -49,19 +62,18 @@ namespace Prog
                 if (line.Contains("VOIDED"))
                 {
                     receiptProductList.RemoveAt(receiptProductList.Count - 1);
+                    continue;
                 }
-                else
+                
+                var match = regex.Match(line);
+                var code = match.Value;
+                var price = Convert.ToDecimal(match.NextMatch().Value);
+
+                receiptProductList.Add(new Product
                 {
-                    var match = regex.Match(line);
-                    var code = match.Value;
-                    var price = Convert.ToDecimal(match.NextMatch().Value);
-                    
-                    receiptProductList.Add(new Product
-                    {
-                        Code = code,
-                        Price = price
-                    }); 
-                }
+                    Code = code,
+                    Price = price
+                });
             }
 
             return receiptProductList;
@@ -71,5 +83,6 @@ namespace Prog
         {
             return await File.ReadAllLinesAsync(path);
         }
+
     }
 }
--- Sln/Prog/Program.cs
+++ Sln/Prog/Program.cs
@@ -6,13 +6,28 @@ namespace Prog
     {
         static void Main(string[] args)
         {
+            string dir;
+
+            if (args.Length > 0 && args[0] != null)
+            {
+                dir = args[0];
+            }
+            else
+            {
+                dir = "C:/data/";
+            }
+            
+            var priceFilePath = $"{dir}prices.csv";
+
             //Read Product List From File
-
-            //Read Recipts From directory
-
-            //Claculate Differences
+            var productList = Product.ReadPricesFromFile(priceFilePath).Result;
+            //Read Receipts From directory
+            var productsFromReceipts = Product.ReadReceiptFromFile(dir).Result;
+            //Calculate Differences
 
             //Write Report         
         }
+
+        
     }
 }
