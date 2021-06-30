a1d3c52ab2068c72d17f9ed703614906a324bc90
Clayton Reeves <Clayton.reeves@utahtechsolutions.com>
2021-06-29 22:11:56 -0500

    Failing test

---
+++ Sln/Tests/WrongPriceProductProcessing.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Prog;
+
+namespace Tests
+{
+    public class WrongPriceProductProcessing
+    {
+        [Fact]
+        public void EmptyPriceAndReceipts()
+        {
+            var prices = new List<Product>();
+            var receipts = new List<Product>();
+
+            WrongPriceProduct.FindImproperlyPricedProducts(prices, receipts);
+        }
+    }
+}
